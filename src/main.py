6b62be9bb22014aefd57c498d5dd83f7974fbfc7
Luís Tchitue <260276477+luistchitue97@users.noreply.github.com>
2026-07-31 19:38:59 +0000

    Refactor main.py to simplify input handling and update model input format

--- src/main.py
+++ src/main.py
@@ -2,27 +2,23 @@ import torch
 
 from data.dataloader import dataloader
 from attention.multi_head_attention import MultiHeadAttention
+from model.gpt import GPTModel
+from utils.model_config import GPT_CONFIG_124M
 
-iterator = iter(dataloader())
-inputs, targets = next(iterator)
-print("Inputs:", inputs)
-print("Targets:", targets)
+
+#iterator = iter(dataloader())
+model = GPTModel(GPT_CONFIG_124M)
+#inputs, targets = next(iterator)
+#print("Inputs:", inputs)
+#print("Targets:", targets)
 
 
 inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], 
-   [0.55, 0.87, 0.66], 
-   [0.57, 0.85, 0.64], 
-   [0.22, 0.58, 0.33], 
-   [0.77, 0.25, 0.10], 
-   [0.05, 0.80, 0.55]] 
+    [[1, 2, 3, 4]],
+    dtype=torch.long,
 )
 
-batch = torch.stack([inputs, inputs], dim=0)  # Create a batch of size 2
-
-batch_size, cont_length, in_dim = batch.shape
-d_out = 4
-mha = MultiHeadAttention(in_dim=in_dim, out_dim=d_out, cont_length=cont_length, num_heads=2, dropout=0.0)
-context_vec = mha(batch)    
-print("Context Vector:", context_vec)
-print("Context Vector Shape:", context_vec.shape)
+out = model(inputs)
+print("Input batch:\n", inputs)
+print("Output shape:", out.shape)
+print(out)
