ae06e4d5cfd4db657494c315ff36251e234cd335
Luís Tchitue <260276477+luistchitue97@users.noreply.github.com>
2026-07-31 17:49:15 +0000

    Implement MultiHeadAttention class and integrate it into the main script for context vector computation

--- src/attention/multi_head_attention.py
+++ src/attention/multi_head_attention.py
@@ -1,5 +1,53 @@
-"""Multi-head attention placeholder."""
+import torch
+import torch.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, in_dim, out_dim, cont_length, num_heads, qkv_bias=False, dropout=0.1):
+        super().__init__()
+        assert out_dim % num_heads == 0, "out_dim must be divisible by num_heads"
+
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.num_heads = num_heads
+        self.head_dim = out_dim // num_heads
 
 
-def multi_head_attention():
-    raise NotImplementedError
+        # dropout layer for attention weights
+        self.attn_dropout = nn.Dropout(dropout)
+
+        # projection matrices:
+        self.W_key = nn.Linear(in_dim, out_dim, bias=qkv_bias)
+        self.W_query = nn.Linear(in_dim, out_dim, bias=qkv_bias)
+        self.W_value = nn.Linear(in_dim, out_dim, bias=qkv_bias)        
+
+        # mask for attention to prevent attending to future tokens
+        self.register_buffer("mask", torch.triu(torch.ones(cont_length, cont_length), diagonal=1))
+
+        self.out_proj = nn.Linear(out_dim, out_dim)
+
+    def forward(self, x):
+        b, cont_length, d_in = x.size()
+
+        # projections
+        queries = self.W_query(x)
+        keys = self.W_key(x) 
+        values = self.W_value(x)
+
+        queries = queries.view(b, cont_length, self.num_heads, self.head_dim)
+        keys = keys.view(b, cont_length, self.num_heads, self.head_dim)
+        values = values.view(b, cont_length, self.num_heads, self.head_dim)
+
+        queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        attn_scores = attn_scores.masked_fill(self.mask[:cont_length, :cont_length] == 1, -torch.inf)
+
+        #attn_weights = attn_weights.masked_fill(self.mask[:cont_length, :cont_length] == 1, float('-inf'))
+        attn_weights = torch.softmax(attn_scores / (keys.shape[-1] ** 0.5), dim=-1)
+        attn_weights = self.attn_dropout(attn_weights)
+        context_vec = (attn_weights @ values).transpose(1, 2).contiguous().view(b, cont_length, self.out_dim)
+
+        context_vec = self.out_proj(context_vec)
+        return context_vec
--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
+import torch
+
 from data.dataloader import dataloader
+from attention.multi_head_attention import MultiHeadAttention
 
 iterator = iter(dataloader())
 inputs, targets = next(iterator)
 print("Inputs:", inputs)
 print("Targets:", targets)
+
+
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], 
+   [0.55, 0.87, 0.66], 
+   [0.57, 0.85, 0.64], 
+   [0.22, 0.58, 0.33], 
+   [0.77, 0.25, 0.10], 
+   [0.05, 0.80, 0.55]] 
+)
+
+batch = torch.stack([inputs, inputs], dim=0)  # Create a batch of size 2
+
+batch_size, cont_length, in_dim = batch.shape
+d_out = 4
+mha = MultiHeadAttention(in_dim=in_dim, out_dim=d_out, cont_length=cont_length, num_heads=2, dropout=0.0)
+context_vec = mha(batch)    
+print("Context Vector:", context_vec)
+print("Context Vector Shape:", context_vec.shape)
