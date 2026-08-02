31186808c2e3e0bc479683f8de8a6d496295882f
Luís Tchitue <260276477+luistchitue97@users.noreply.github.com>
2026-08-02 19:38:13 +0000

    Add training and evaluation functions in trainer.py; implement text tokenization and sample generation

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -3,6 +3,7 @@ from data.dataloader import create_dataloader
 from model.gpt import GPTModel
 from utils.model_config import GPT_CONFIG_124M
 from data.the_veredict import RAW_TEXT as text_data
+from inference.generate import generate_text as generate_text_simple
 
 
 ######## TRAINING STARTS HERE ########
@@ -12,6 +13,17 @@ train_data = text_data[:split_idx]
 val_data = text_data[split_idx:]
 
 torch.manual_seed(123)
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)  # Add batch dimension
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    token_ids_list = token_ids.squeeze(0).tolist()  # Remove batch dimension and convert to list
+    decoded_text = tokenizer.decode(token_ids_list)
+    return decoded_text
+
 
 train_loader = create_dataloader(
     txt=train_data,
@@ -64,3 +76,69 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
             break
     return total_loss / num_batches
 
+
+def train_model_simple(model, train_loader, val_loader,
+                       optimizer, device, num_epochs, eval_freq, eval_iter,
+                       start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch,
+                target_batch,
+                model,
+                device
+            )
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}, "
+                )
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
+
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model,
+            idx=encoded,
+            max_new_tokens=50,
+            context_size=context_size
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))    
+    model.train()
